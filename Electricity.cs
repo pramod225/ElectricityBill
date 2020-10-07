b781c07473b6459550c392dd2a725e223c573696
BaniYashu <BaniYashu@DESKTOP-C3CMV99>
2020-10-07 17:56:18 +0530

    initial

--- Electricity.cs
+++ Electricity.cs
@@ -10,15 +10,16 @@ namespace Calculatebill
    public  class ElectricityBill
     {
         int CustomerId,Unit;
-        string Name;
+        string  CustomerName;
         double Charge,Amount,SurchageAmount,NetAmount;
 
+               
         public void GetCustomerDetails()
         {
             Console.WriteLine("Enter Customer id");
             CustomerId = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine("Enter Customer's Name");
-            Name =Console.ReadLine();
+            CustomerName =Console.ReadLine();
             Console.WriteLine("Enter Unit ");
             Unit = Convert.ToInt16(Console.ReadLine());
         }
@@ -27,44 +28,37 @@ namespace Calculatebill
             if (Unit > 600)
             {
                 Charge = 2.00;
-                Amount = (Charge * Unit);
-                SurchageAmount = (Amount * 15) / 100;
-                NetAmount = Amount + SurchageAmount;
+               
             }
             else if (Unit > 200 && Unit < 400)
             {
                 Charge = 1.50;
-                Amount = (Charge * Unit);
-                SurchageAmount = (Amount * 15) / 100;
-                NetAmount = Amount + SurchageAmount;
             }
             else if (Unit > 400 && Unit < 600)
             {
                 Charge = 1.80;
-                Amount = (Charge * Unit);
-                SurchageAmount = (Amount * 15) / 100;
-                NetAmount = Amount + SurchageAmount;
             }
             else
             {
                 Charge = 1.20;
-                Amount = (Charge * Unit);
-                SurchageAmount = (Amount * 15) / 100;
-                NetAmount = Amount + SurchageAmount;
             }
         }
-
+        void CalculateBill()
+        {
+            Amount = (Charge * Unit);
+            SurchageAmount = (Amount * 15) / 100;
+            NetAmount = Amount + SurchageAmount;
+        }
            public  void ShowAmountDetais()
             {
                 CalculateAmount();
+                CalculateBill();
                 Console.WriteLine("Customer's Id:{0}",CustomerId);
-                Console.WriteLine("Customer's Name:{0}", Name);
+                Console.WriteLine("Customer's Name:{0}", CustomerName);
                 Console.WriteLine("Cusumed Unit of Electricity :{0}", Unit);
                 Console.WriteLine("Amount charges @Rs.{0} per Unit{1}:",Charge,Amount );
                 Console.WriteLine("Surchage Amount:{0}",SurchageAmount);
                 Console.WriteLine("Net Amount Paid By the Customer:{0}", NetAmount);
-
-
             }
         
         }
